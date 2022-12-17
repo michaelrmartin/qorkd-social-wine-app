578de324ecb148e300137baabc34009447a79012
michaelrmartin <iammichaelmartin@gmail.com>
2022-12-17 12:51:04 -0600

    Adds Wine model migration

---
+++ db/migrate/20221217185051_create_wines.rb
@@ -0,0 +1,20 @@
+class CreateWines < ActiveRecord::Migration[7.0]
+  def change
+    create_table :wines do |t|
+      t.string :name
+      t.string :blend
+      t.decimal :price, precision: 5, scale: 2
+      t.integer :origin_id
+      t.string :style
+      t.string :photo_url
+      t.boolean :vegan
+      t.boolean :sparkling
+      t.string :profile
+      t.text :description
+      t.string :color
+      t.string :sweet
+
+      t.timestamps
+    end
+  end
+end
