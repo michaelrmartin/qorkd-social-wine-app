098b2c5c4440736faee003da80a54612ad4ae2bb
michaelrmartin <iammichaelmartin@gmail.com>
2022-12-28 15:21:32 -0600

    Adds Post model and controller

---
+++ db/migrate/20221228211904_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.text :description
+      t.integer :user_id
+      t.integer :wine_id
+      t.string :post_img_url
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_173707) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_211904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "origins", force: :cascade do |t|
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
+    t.integer "wine_id"
+    t.string "post_img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
