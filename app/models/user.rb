cca751486696f5c766ef6aa43a4f4565d8c61eb3
michaelrmartin <iammichaelmartin@gmail.com>
2023-04-04 13:43:18 -0500

    Adds overall rating method to wine model and updates views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  
 
   has_one :user_profile
   has_many :posts
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -7,5 +7,26 @@ class Wine < ApplicationRecord
   belongs_to :origin
   has_many :posts
 
+  def overall_rating
+    total = 0
+    if self.posts.length > 0
+      self.posts.each do |post|
+        total += post.rating
+      end
+    else
+      return 0
+    end
+    return (total / self.posts.length).round(2)
+  end
+
+  def user_posts(user)
+    count = 0
+    self.posts.each do |post|
+      if post.user_id == user.id
+        count += 1
+      end
+    end
+    count
+  end
 
 end
