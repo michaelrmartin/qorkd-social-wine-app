02cb9810f2cfab704e7e9a4d5ef05b0866c9e086
michaelrmartin <iammichaelmartin@gmail.com>
2023-04-05 17:12:23 -0500

    Adds follows controller and model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,17 @@ class UsersController < ApplicationController
 
   def index
     
-    users = User.all
+    @users = User.all
 
-    render json: users.as_json
+    render template: "users/index"
 
   end
 
   def show 
     user_id = params["id"]
-    user = User.find_by(id: user_id)
+    @user = User.find_by(id: user_id)
 
-    render json: user.as_json
+    render template: "users/show"
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,40 @@ class User < ApplicationRecord
   has_many :posts
   has_many :wines, through: :posts
 
+  def recommendations
+
+    data = [
+      {user_id: 1, item_id: 1, rating: 5},
+      {user_id: 1, item_id: 2, rating: 4},
+      {user_id: 1, item_id: 3, rating: 3},
+      {user_id: 1, item_id: 4, rating: 2},
+      {user_id: 2, item_id: 1, rating: 1},
+      {user_id: 2, item_id: 2, rating: 2},
+      {user_id: 2, item_id: 3, rating: 3},
+      {user_id: 2, item_id: 4, rating: 4},{user_id: 3, item_id: 1, rating: 3},
+      {user_id: 3, item_id: 2, rating: 2},
+      {user_id: 3, item_id: 3, rating: 5},
+      {user_id: 3, item_id: 4, rating: 4},
+      {user_id: 3, item_id: 5, rating: 4},
+      {user_id: 4, item_id: 1, rating: 4},
+      {user_id: 4, item_id: 2, rating: 3},
+      {user_id: 4, item_id: 3, rating: 2},
+      {user_id: 4, item_id: 4, rating: 5},
+      {user_id: 4, item_id: 6, rating: 4},
+    ]
+
+    recommender = Disco::Recommender.new
+
+    recommender.fit(data)
+
+    user_recs = recommender.user_recs(3)
+    item_recs = recommender.item_recs(1)
+    similar_users = recommender.similar_users(3)
+
+    return {user_recs: user_recs, item_recs: item_recs, similar_users: similar_users}
+
+  end
+
+
+
 end
