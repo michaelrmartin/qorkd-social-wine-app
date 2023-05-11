268a7fa90c8ee952ead53465d6153897dd642ec5
michaelrmartin <iammichaelmartin@gmail.com>
2023-05-11 15:24:21 -0500

    Updates cors to accept requests from react frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins "localhost:5173", "https://radiant-clafoutis-55f17c.netlify.app/"
 
     resource "*",
       headers: :any,
